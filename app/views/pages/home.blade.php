e3a7cc9e4c61411b0735c6c31a6978b969ef70be
Manu <bionikspoon@gmail.com>
2015-01-17 23:32:03 -0600

    setup homepage and basic layout stuff

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -3,6 +3,13 @@
     Welcome to Larabook!
 @stop
 @section('content')
-    Welcome to Larabook!
+    <!-- Main component for a primary marketing message or call to action -->
+    <div class="jumbotron">
+        <h1>Welcome to Larabook!</h1>
+        <p>Welcome tot he primer place to talk about laravel with others. Why don't you sign up to see what the fuss is about?</p>
+        <p>
+            <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Sign up!</a>
+        </p>
+    </div>
 @stop
 
