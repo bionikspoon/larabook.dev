cbc56aa1848f8851745ab41cca22daba587db332
Manu <bionikspoon@gmail.com>
2015-01-17 22:29:46 -0600

    finished setup

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'host'      => getenv('DB_HOST'),
+			'database'  => getenv('DB_NAME'),
+			'username'  => getenv('DB_USERNAME'),
+			'password'  => getenv('DB_PASSWORD'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
@@ -82,7 +82,6 @@ return array(
 			'password' => '',
 			'prefix'   => '',
 		),
-
 	),
 
 	/*
