2274bb53e321a4c92ff79acf49b64f5379428297
Manu <bionikspoon@gmail.com>
2015-01-20 02:19:08 -0600

    login functional test, sessions controller

--- tests/_support/FunctionalHelper.php
+++ tests/_support/FunctionalHelper.php
@@ -1,10 +1,21 @@
 <?php
 namespace Codeception\Module;
 
+use Laracasts\TestDummy\Factory as TestDummy;
+
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
+/**
+ * Class FunctionalHelper
+ *
+ * @package Codeception\Module
+ */
 class FunctionalHelper extends \Codeception\Module
 {
 
+    public function haveAnAccount()
+    {
+        $user = TestDummy::create('Larabook\Users\User');
+    }
 }
